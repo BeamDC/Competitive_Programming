7424554423c257b91ec2c663963ef4fa0b47719e
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-27 16:41:40 -0500

    Create J1.cpp

---
+++ CCC/2002/J1.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define nl '\n'
+#define all(x) x.begin(), x.end()
+#define vi vector<int>
+#define um unordered_map
+#define all(x) x.begin(), x.end()
+#define print1D(vec) cout<<#vec<<":\n";for(auto x:vec){cout<<x<<" ";}cout<<nl;
+
+/*****************************************************************************/
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+
+    unsigned int n;cin>>n;
+    switch (n){
+        case 0:
+            cout<<" * * *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<""<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<" * * *"<<nl;
+            break;
+        case 1:
+            cout<<""<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<""<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<""<<nl;
+            break;
+        case 2:
+            cout<<" * * *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<" * * *"<<nl;
+            cout<<"*"<<nl;
+            cout<<"*"<<nl;
+            cout<<"*"<<nl;
+            cout<<" * * *"<<nl;
+            break;
+        case 3:
+            cout<<" * * *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<" * * *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<" * * *"<<nl;
+            break;
+        case 4:
+            cout<<""<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<" * * *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<""<<nl;
+            break;
+        case 5:
+            cout<<" * * *"<<nl;
+            cout<<"*"<<nl;
+            cout<<"*"<<nl;
+            cout<<"*"<<nl;
+            cout<<" * * *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<" * * *"<<nl;
+            break;
+        case 6:
+            cout<<" * * *"<<nl;
+            cout<<"*"<<nl;
+            cout<<"*"<<nl;
+            cout<<"*"<<nl;
+            cout<<" * * *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<" * * *"<<nl;
+            break;
+        case 7:
+            cout<<" * * *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<""<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<""<<nl;
+            break;
+        case 8:
+            cout<<" * * *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<" * * *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<" * * *"<<nl;
+            break;
+        case 9:
+            cout<<" * * *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<"*     *"<<nl;
+            cout<<" * * *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<"      *"<<nl;
+            cout<<" * * *"<<nl;
+            break;
+    }return 0;
+}
+/*****************************************************************************/
