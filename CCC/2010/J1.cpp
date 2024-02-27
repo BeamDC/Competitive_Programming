c3163fd77acea8147070c2c9bbb6a77b5ca6eda3
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-27 16:08:15 -0500

    Create J1.cpp

---
+++ CCC/2010/J1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i;
+    scanf("%d", &i);
+    if(i==1)printf("%d",1);
+    else if(i==2)printf("%d",2);
+    else if(i==3)printf("%d",2);
+    else if(i==4)printf("%d",3);
+    else if(i==5)printf("%d",3);
+    else if(i==6)printf("%d",3);
+    else if(i==7)printf("%d",2);
+    else if(i==8)printf("%d",2);
+    else if(i==9)printf("%d",1);
+    else if(i==10)printf("%d",1);
+}
