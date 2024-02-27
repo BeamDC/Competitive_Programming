bb051decf2182da2b3730d7814ef5e226d3b8368
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-27 12:10:24 -0500

    Create J1.cpp

---
+++ CCC/2020/J1.cpp
@@ -0,0 +1,16 @@
+include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+    int s,m,l, t;
+    string feels;
+
+    cin >> s >> m >> l;
+
+    t = s+(m*2)+(l*3);
+
+    feels = (t<10) ? "sad" : "happy";
+    cout << feels;
+
+    return 0;
+}
