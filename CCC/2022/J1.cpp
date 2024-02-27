595cf1adf3188cca29ecac1dc061f76de78dfe52
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-27 11:59:01 -0500

    Update J1.cpp

--- CCC/2022/J1.cpp
+++ CCC/2022/J1.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+int main() {
+    int r = 0;
+    int s = 0;
 
+    std::cin >> r;
+    std::cin >> s;
+
+    r *= 8;
+    s *= 3;
+
+    std::cout << (r+s) - 28;
+    return 0;
+}
