f957dc058f29f67698c4c58e5c63061653f47104
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-28 10:47:15 -0500

    Update S1.cpp

--- CCC/2024/S1.cpp
+++ CCC/2024/S1.cpp
@@ -1,23 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define nl '\n'
-#define all(x) x.begin(), x.end()
-#define vi vector<int>
-#define um unordered_map
-#define all(x) x.begin(), x.end()
-#define print1D(vec) cout<<#vec<<":\n";for(auto x:vec){cout<<x<<" ";}cout<<nl;
 
-/*****************************************************************************/
+int h[1000001];
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
 
-    int n,p,t=0;cin>>n;
-    p=n/2;
-    int h[n];
-    for (auto& x : h)cin>>x;
-    for (int i = 0; i < p; ++i) {
-        if(h[i]==h[i+p])++t;
+    int n,t=0;cin>>n;
+    for (int i=0; i<n;++i)cin>>h[i];
+    for (int i = 0; i < n/2; ++i) {
+        if(h[i]==h[i+n/2])++t;
     }cout<<t*2;
 }
-/*****************************************************************************/
