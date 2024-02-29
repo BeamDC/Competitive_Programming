e95007a5c28dc497f52fc63a0f728eb58ff58eb3
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-29 14:05:33 -0500

    Update J2.cpp

--- Mock CCC/2022/Contest 2/J2.cpp
+++ Mock CCC/2022/Contest 2/J2.cpp
@@ -5,17 +5,16 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int n,a,b,h,t1=0,t2=0;cin>>n>>a>>b;
+    int n,a,b,x,y,t1=0,t2=0;cin>>n>>a>>b;
     for(int i=0;i<n;++i){
-        cin>>h;
-        if(h<a)t1+=1;
+        cin>>x>>y;
+        if(x<a)t1+=1;
         else t1+=2;
-        cin>>h;
-        if(h<b)t2+=1;
+        if(y<b)t2+=1;
         else t2+=2;
     }
-    if(t1>t2)cout<<"Andrew wins!";
-    else if (t1<t2)cout<<"Tommy wins!";
+    if(t1<t2)cout<<"Andrew wins!";
+    else if (t1>t2)cout<<"Tommy wins!";
     else cout<<"Tie!";
     cout<<'\n';
     return 0;
