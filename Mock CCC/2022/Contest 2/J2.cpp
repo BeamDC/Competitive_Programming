4d7255eb0629b298cf38287b022fe3fecdf3c00e
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-29 13:59:35 -0500

    Create J2.cpp

---
+++ Mock CCC/2022/Contest 2/J2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx2")
+
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n,a,b,h,t1=0,t2=0;cin>>n>>a>>b;
+    for(int i=0;i<n;++i){
+        cin>>h;
+        if(h<a)t1+=1;
+        else t1+=2;
+        cin>>h;
+        if(h<b)t2+=1;
+        else t2+=2;
+    }
+    if(t1>t2)cout<<"Andrew wins!";
+    else if (t1<t2)cout<<"Tommy wins!";
+    else cout<<"Tie!";
+    cout<<'\n';
+    return 0;
+}
