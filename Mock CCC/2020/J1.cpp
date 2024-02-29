dc45d570e7e378a5c7a3e989f8310dab1dcce6ad
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-29 12:26:14 -0500

    Update J1.cpp

--- Mock CCC/2020/J1.cpp
+++ Mock CCC/2020/J1.cpp
@@ -1,1 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int arr[4];
+    cin>>arr[0]>>arr[1]>>arr[2]>>arr[3];
+    sort(arr,arr+4);
+    cout<<arr[0]<<'\n'<<arr[3]<<'\n';
+}
