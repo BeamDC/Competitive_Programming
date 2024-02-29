41bd0904d074fe63ddd4fe3f5c130f9a704284a0
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-29 13:31:40 -0500

    Create J1.cpp

---
+++ Mock CCC/2022/Contest 2/J1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int f,m,b,tf,tm,tb;
+    cin>>f>>m>>b>>tf>>tm>>tb;
+    cout<<(tf + tm + tb)<<' '<<(tf*f + tm*m + tb*b)<<'\n';
+}
