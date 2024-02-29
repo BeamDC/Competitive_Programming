d725a9cc55b9981da68cdb9042babd85c49f46b0
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-29 09:56:55 -0500

    Create J1.cpp

---
+++ CCC/2024/J1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx2")
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    
+    int p,c;
+    cin>>p;
+    c=p*3;
+    cin>>p;
+    c+=p*4;
+    cin>>p;
+    c+=p*5;
+    cout<<c;
+    
+}
