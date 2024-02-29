a3e73b8eae9cbe58f9c93869c2a0c45b07552091
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-02-29 12:11:42 -0500

    Create 16_BIT_SW_ONLY.cpp

---
+++ Uncategorized/DMOJ/16_BIT_SW_ONLY.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {int n;scanf("%d",&n);int a,b,p;
+for(int i=0;i<n;++i){scanf("%d %d %d",&a,&b,&p);
+if(a*b==p)printf("POSSIBLE DOUBLE SIGMA\n");
+else printf("16 BIT S/W ONLY\n");}}
