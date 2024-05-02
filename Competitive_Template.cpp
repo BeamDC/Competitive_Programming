e04ef8c3cbc5a24918f97f94481dace9835b7082
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-05-02 11:01:28 -0400

    Update Competitive_Template.cpp

--- Competitive_Template.cpp
+++ Competitive_Template.cpp
@@ -3,26 +3,21 @@
 using namespace std;
 //optimizations
 #pragma GCC optimize "Ofast"
-#pragma GCC target("avx2")
 #ifdef fread_unlocked
 #define fread fread_unlocked
 #endif
 //macros
 #define all(x) x.begin(), x.end()
 #define IOS ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define umap unordered_map
 //fast I/O
-//the INPUT_SIZE should be as close to the max number of bits as possible
-//since all input digits are chars, calculate the max number of chars
-//then multiply the value by 8 since a char is one byte of memory
-//the result is the max amount of memory needed to solve
-//approximate using bit shift to get 
-//the optimal amount of memory to allocate to the buffer
 #define INPUT_SIZE 32<<20
-int _i0=0;
-char _i[INPUT_SIZE];
+int _c0=0;
+char _c[INPUT_SIZE];
 char _;
-#define fin_string(s) do{while(true){_=_i[_i0++];if(10<_){s.push_back(_);}else{break;}}}while(0)
-#define finset(p) if(p!="")freopen(p,"r",stdin);fread(_i,1,INPUT_SIZE,stdin)
+#define fin_string(s) do{while(true){_=_c[_c0++];if(10<_){s.push_back(_);}else{break;}}}while(0)
+#define finset(p) freopen(p,"r",stdin);fread(_c,1,INPUT_SIZE,stdin)
+#define finset_nf fread(_c,1,INPUT_SIZE,stdin)
 
 //this needs to still be tested, no idea if it works the way i want it to lol
 template<typename T>
@@ -65,9 +60,8 @@ vector<string> parseln<string>(char delimiter) {
 }
 /*****************************************************************************/
 int main() {
+    IOS;
     finset("Input.txt");
-    //input format (for ints, space delimiter):
-    //vector<int> = parseln<int>(' ');
     return 0;
 }
 /*****************************************************************************/
