08a004c95a36769cf718bf66a5ac51453bc75b92
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-05-15 13:27:55 -0400

    Create J1.rs

---
+++ Woburn Challenge/2017/Contest 1/J1.rs
@@ -0,0 +1,18 @@
+use std::io;
+macro_rules! parseln {
+    ($($t: ty),+) => ({
+        let mut a_str = String::new();
+        io::stdin().read_line(&mut a_str).expect("read error");
+        let mut a_iter = a_str.split_whitespace();
+        (
+            $(
+            a_iter.next().unwrap().parse::<$t>().expect("parse error"),
+            )+
+        )
+    })
+}
+
+fn main() {
+    let s = parseln!(String).0;
+    println!("{}, eh",s);
+}
