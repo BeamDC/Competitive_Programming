9786a76b52e22432fb6c1a088eb7ddadff2c9a0b
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-05-15 13:17:05 -0400

    Create J1.cpp

---
+++ Woburn Challenge/2016/Contest 1/J1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define um unordered_map
+
+int main() {
+    fast
+    
+    int n;cin>>n;
+    um<int, string> oo = {
+        {2,"oo"},{3,"ooo"},{4,"oooo"},{5,"ooooo"},{6,"oooooo"},
+        {7,"ooooooo"},{8,"oooooooo"},{9,"ooooooooo"},{10,"oooooooooo"},{11,"ooooooooooo"},
+        {12,"oooooooooooo"},{13,"ooooooooooooo"},{14,"oooooooooooooo"},{15,"ooooooooooooooo"},{16,"oooooooooooooooo"},
+        {17,"ooooooooooooooooo"},{18,"oooooooooooooooooo"},{19,"ooooooooooooooooooo"},{20,"oooooooooooooooooooo"}
+    };
+    cout<<"sp"<<oo[n]<<"ky";
+}
