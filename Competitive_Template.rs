cc24b30a58754c57e9a9a00ab29010d066d5e9c0
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-05-17 14:17:51 -0400

    Create Competitive_Template.rs

---
+++ Competitive_Template.rs
@@ -0,0 +1,51 @@
+use std::io;
+
+macro_rules! parseln { //known amount of values, different types eg.(f64,String,i8)
+    ($($t: ty),+) => ({
+        let mut a_str = String::new();
+        io::stdin().read_line(&mut a_str).expect("read error");
+        let mut a_iter = a_str.split_whitespace();
+        (
+            $(
+            a_iter.next().unwrap().parse::<$t>().expect("parse error"),
+            )+
+        )
+    })
+}
+
+macro_rules! parseun { //unkown amout of values, one type eg.(u32,u32,u32)
+    ($t:ty) => {{
+        let mut a_str = String::new();
+        io::stdin().read_line(&mut a_str).expect("read error");
+        let a_iter = a_str.split_whitespace();
+        a_iter.map(|s| s.parse::<$t>().expect("parse error")).collect::<Vec<_>>()
+    }};
+}
+
+//find gcd of two nums
+fn gcd(mut a:u64,mut b:u64) -> u64{
+    if a == 0 {
+        return b;
+    } else if b == 0 {
+        return a;
+    }
+    let i = a.trailing_zeros();
+    let j = b.trailing_zeros();
+    let k:u32;
+    if i<j{k=i}
+    else {k=j}
+    a >>= i;
+    b >>= j;
+    loop {
+        if a > b {
+            a^=b;b^=a;a^=b;
+        }
+        b -= a;
+        if b == 0 {
+            return a << k;
+        }
+        b >>= b.trailing_zeros();
+    }
+}
+
+fn main() {}
