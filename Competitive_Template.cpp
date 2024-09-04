d2925bdee52b752d54401d391eddb8f26e583d57
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-09-04 12:13:20 -0400

    Update Competitive_Template.cpp

--- Competitive_Template.cpp
+++ Competitive_Template.cpp
@@ -5,8 +5,6 @@ using namespace std;
 #define umap unordered_map
 #define uset unordered_set
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
 /********************************************************* @robhoma was here */
 int main() {
     cin.tie(nullptr)->sync_with_stdio(0);
