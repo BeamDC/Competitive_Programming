2b1f24e959622bb4d72564ee030771e1e4e019a6
BIIN <116185629+BeamDC@users.noreply.github.com>
2025-01-20 11:00:28 -0500

    Update P1.c

--- UTS Open/2024/P1.c
+++ UTS Open/2024/P1.c
@@ -1,1 +1,12 @@
+#include <math.h>
+#include <stdio.h>
 
+int main(void) {
+    long to_water, water;
+    long dist = 0;
+    scanf("%ld\n%ld", &to_water, &water);
+    int gap = ceil((float) to_water / (float)water);
+    dist = 2 * to_water * gap - water * gap * (gap - 1);
+    printf("%ld\n", dist);
+    return 0;
+}
